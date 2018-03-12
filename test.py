d85fbb66f53164aa50f19a55f1078b5d829f3b89
S <=>
2018-03-12 15:33:18 -0700

    updated print

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import populartimes
 
+res = populartimes.get("AIzaSyCTmst6SvsOAQanZKNt-2pt6nuLoFf2kSA", ["bar"],(48.132986, 11.566126), (48.142199, 11.580047))
 
-populartimes.get("AIzaSyCTmst6SvsOAQanZKNt-2pt6nuLoFf2kSA", ["bar"],(48.132986, 11.566126), (48.142199, 11.580047))
+print res
+
